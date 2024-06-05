9154609cb5afb5c83b3cb6b4be1e3a058f701d42
jeongban <cox82910@naver.com>
2024-06-06 01:31:33 +0900

    feat : javagui ready

---
+++ src/Gui/GuiProject.java
@@ -0,0 +1,23 @@
+package Gui;
+import javax.swing.*;
+//격투 싸움 gui
+//선수1 hp(100) 주먹공격(10~20) 발차기(횟수 3번)(10~30)
+//선수2 hp(100) 주먹공격(10~20) 발차기(횟수 3번)(10~30)
+//버튼으로 공격기능만들기 발차기는 횟수 차감
+//hp가 100 미만이면 경기종료
+
+
+
+class Gui {
+    JPanel jp = new JPanel();//panel생성
+
+
+
+}
+
+public class GuiProject extends JFrame {
+    public static void main(String[] args) {
+
+    }
+
+}
